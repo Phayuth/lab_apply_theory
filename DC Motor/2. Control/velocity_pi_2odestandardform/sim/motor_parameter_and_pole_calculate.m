f8a7985c51f90aca179d193f1f51b164fc40a44c
Phayuth <70325209+Phayuth@users.noreply.github.com>
2022-09-09 11:53:19 +0900

    update

--- DC Motor/2. Control/velocity_pi_2odestandardform/sim/motor_parameter_and_pole_calculate.m
+++ DC Motor/2. Control/velocity_pi_2odestandardform/sim/motor_parameter_and_pole_calculate.m
@@ -6,7 +6,11 @@ b = 17.35;
 c = 5.635;
 
 wn = 2*pi*6;
-zeta = 1.3;
+zeta = 1;
 
 ki = wn^2/b
 kp = (2*zeta*wn-a)/b
+
+
+wnf = 2*pi*10
+zetaf = 1
---
+++ Mobile Robot Control/diffdrive_bkstp_ctrl_rrts_with_PI_velo_controller/calculate_kp_ki.m
@@ -0,0 +1,16 @@
+r_a = 30.17;
+r_b = 18.18;
+r_c = 5.71;
+
+l_a = 26.65;
+l_b = 15.16;
+l_c = 5.48;
+
+wn = 2*pi*6;
+zeta = 1;
+
+r_ki = wn^2/r_b
+r_kp = (2*zeta*wn-r_a)/r_b
+
+l_ki = wn^2/l_b
+l_kp = (2*zeta*wn-l_a)/l_b
